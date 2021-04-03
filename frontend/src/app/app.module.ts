496c7376194c4ad0e23f94823c1834a77f69c0c6
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-04-03 22:30:04 +0200

    adiing upper-panel component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpperPanelComponent } from './upper-panel/upper-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpperPanelComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/upper-panel/upper-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upper-panel',
+  templateUrl: './upper-panel.component.html',
+  styleUrls: ['./upper-panel.component.scss']
+})
+export class UpperPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
