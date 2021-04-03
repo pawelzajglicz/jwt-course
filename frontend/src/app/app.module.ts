4a63098314ee663924a907430f25522c0baccb14
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-04-03 22:57:25 +0200

    adding navigation component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavigationComponent } from './navigation/navigation.component';
 import { UpperPanelComponent } from './upper-panel/upper-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationComponent,
     UpperPanelComponent
   ],
-  imports: [
-    BrowserModule,
+  imports: [,
     AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
