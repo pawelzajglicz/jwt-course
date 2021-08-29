fbd0720ce559ba500ca7845622d756e6210aba8e
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-08-29 13:47:09 +0200

    making user class an entity

--- backend/src/main/java/com/nimuairy/auth/domain/User.java
+++ backend/src/main/java/com/nimuairy/auth/domain/User.java
@@ -1,10 +1,23 @@
 package com.nimuairy.auth.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+@AllArgsConstructor
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String userId;
     private String firstName;
@@ -16,7 +29,9 @@ public class User implements Serializable {
     private Date lastLoginDate;
     private Date lastLoginDateDisplay;
     private Date joinDate;
+    @Transient
     private String[] roles;
+    @Transient
     private String[] authorities;
     private boolean isActive;
     private boolean isNotLocked;
