ea6c79e8abc7013b21a45e681149604cef62ee53
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-08-29 17:44:04 +0200

    renaing resource into controller

--- backend/src/main/java/com/nimuairy/auth/controller/UserController.java
+++ backend/src/main/java/com/nimuairy/auth/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.nimuairy.auth.resource;
+package com.nimuairy.auth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
-public class UserResource {
+public class UserController {
 
     @GetMapping("/home")
     public String showUser() {
