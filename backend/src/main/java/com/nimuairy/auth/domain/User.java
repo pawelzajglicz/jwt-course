cafe215a586086ed929d49a93810ed47358123ee
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-08-29 13:25:44 +0200

    adding user class

---
+++ backend/src/main/java/com/nimuairy/auth/domain/User.java
@@ -0,0 +1,23 @@
+package com.nimuairy.auth.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String[] roles;
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+}
