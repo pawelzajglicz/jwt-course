b0c5a874ae57b73989a8afd01f2eae243ec942f9
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-01 23:39:44 +0200

    adding UserRepository

---
+++ backend/src/main/java/com/nimuairy/auth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.nimuairy.auth.repository;
+
+import com.nimuairy.auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserByUsername(String username);
+
+    User findUserByEmail(String email);
+}
