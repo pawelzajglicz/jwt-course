6c4a6cb08c47e4d6629ebb9c281e0986acfbdb29
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-01 23:07:57 +0200

    fixing JwtAuthenticationEntryPoint

--- backend/src/main/java/com/nimuairy/auth/filter/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/nimuairy/auth/filter/JwtAuthenticationEntryPoint.java
@@ -2,21 +2,20 @@ package com.nimuairy.auth.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimuairy.auth.domain.HttpResponse;
-
-import static com.nimuairy.auth.constant.SecurityConstant.*;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import liquibase.pro.packaged.O;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
-
+import static com.nimuairy.auth.constant.SecurityConstant.FORBIDDEN_MESSAGE;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Component
 public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
 
     @Override
