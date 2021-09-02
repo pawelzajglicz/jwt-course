b0cdef89abb39e2edf9b41c0f01401a1444301f0
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-02 02:20:58 +0200

    adding custom exceptions

---
+++ backend/src/main/java/com/nimuairy/auth/exception/domain/UsernameExistException.java
@@ -0,0 +1,8 @@
+package com.nimuairy.auth.exception.domain;
+
+public class UsernameExistException extends Exception {
+
+    public UsernameExistException(String message) {
+        super(message);
+    }
+}
