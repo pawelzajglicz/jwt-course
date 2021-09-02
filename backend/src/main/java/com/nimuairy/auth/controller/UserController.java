e70b62e6b7a9771e0e43bba4e0ff35523d94fa20
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-02 11:29:14 +0200

    adding exception handling

--- backend/src/main/java/com/nimuairy/auth/controller/UserController.java
+++ backend/src/main/java/com/nimuairy/auth/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.nimuairy.auth.controller;
 
+import com.nimuairy.auth.exception.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
-public class UserController {
+public class UserController extends ExceptionHandling {
 
     @GetMapping("/home")
     public String showUser() {
