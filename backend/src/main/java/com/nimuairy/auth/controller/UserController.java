16e9a8d61c33a99286351c7dc69f5b857733924b
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-06 22:43:40 +0200

    adding login endpoint

--- backend/src/main/java/com/nimuairy/auth/controller/UserController.java
+++ backend/src/main/java/com/nimuairy/auth/controller/UserController.java
@@ -1,23 +1,44 @@
 package com.nimuairy.auth.controller;
 
 import com.nimuairy.auth.domain.User;
+import com.nimuairy.auth.domain.UserPrincipal;
 import com.nimuairy.auth.exception.ExceptionHandling;
 import com.nimuairy.auth.exception.domain.EmailExistException;
 import com.nimuairy.auth.exception.domain.UserNotFoundException;
 import com.nimuairy.auth.exception.domain.UsernameExistException;
 import com.nimuairy.auth.service.UserService;
+import com.nimuairy.auth.utility.JWTTokenProvider;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import static com.nimuairy.auth.constant.SecurityConstant.JWT_TOKEN_HEADER;
 
 @RestController
 @RequestMapping(path = {"/", "/user"})
 public class UserController extends ExceptionHandling {
 
+    private AuthenticationManager authenticationManager;
+    private JWTTokenProvider jwtTokenProvider;
     private UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
         this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticate(user.getUsername(), user.getPassword());
+        User loginUser = userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+
+        return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
     }
 
     @PostMapping("/register")
@@ -25,5 +46,16 @@ public class UserController extends ExceptionHandling {
         User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
 
         return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(userPrincipal));
+
+        return headers;
     }
 }
