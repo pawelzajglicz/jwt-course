860785bf1769a7b081f5d51bd7eedb2029308e76
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-13 22:55:29 +0200

    adding authentication success listener

---
+++ backend/src/main/java/com/nimuairy/auth/listener/AuthenticationSuccessListener.java
@@ -0,0 +1,26 @@
+package com.nimuairy.auth.listener;
+
+import com.nimuairy.auth.domain.User;
+import com.nimuairy.auth.service.LoginAttemptService;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationSuccessListener {
+
+    private LoginAttemptService loginAttemptService;
+
+    public AuthenticationSuccessListener(LoginAttemptService loginAttemptService) {
+        this.loginAttemptService = loginAttemptService;
+    }
+
+    @EventListener
+    public void onAuthenticationSuccess(AuthenticationSuccessEvent event) {
+        Object principal = event.getAuthentication().getPrincipal();
+        if (principal instanceof User) {
+            User user = (User) principal;
+            loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
+        }
+    }
+}
