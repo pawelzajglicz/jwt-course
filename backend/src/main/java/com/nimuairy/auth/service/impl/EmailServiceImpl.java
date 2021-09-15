1e28e9727870cb6dc6823c82a534c8b4e8a20af0
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-15 21:46:24 +0200

    creating email content

--- backend/src/main/java/com/nimuairy/auth/service/impl/EmailServiceImpl.java
+++ backend/src/main/java/com/nimuairy/auth/service/impl/EmailServiceImpl.java
@@ -1,14 +1,40 @@
 package com.nimuairy.auth.service.impl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import static com.nimuairy.auth.constant.EmailConstant.*;
 
 @Service
 public class EmailServiceImpl implements com.nimuairy.auth.service.EmailService {
+
+    @Value("${cc-email}")
+    private String ccEmail;
+
+    @Value("${from-email}")
+    private String fromEmail;
+
+    private Message createEmail(String firstname, String password, String email) throws MessagingException {
+        Message message = new MimeMessage(getEmailSession());
+        message.setFrom(new InternetAddress(fromEmail));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email, false));
+        message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(ccEmail, false));
+        message.setSubject(EMAIL_SUBJECT);
+        message.setSubject("Hello " + firstname + ", \n \n Your new account password is: " + password + "\n \n The Support Team");
+        message.setSentDate(new Date());
+        message.saveChanges();
+
+        return message;
+    }
 
     private Session getEmailSession() {
         Properties properties = System.getProperties();
