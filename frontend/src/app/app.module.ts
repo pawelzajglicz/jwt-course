726d7fad2ca401bcad572df8872b0d685e83dca9
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-20 22:39:10 +0200

    adding HttpModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -14,7 +15,8 @@ import { UpperPanelComponent } from './upper-panel/upper-panel.component';
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
