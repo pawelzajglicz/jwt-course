a9dd2678bedce4745369057007ffc67fc8b57278
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-23 22:19:38 +0200

    adding notification module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AuthInterceptor } from './auth/interceptor/auth.interceptor';
 import { AuthenticationService } from './auth/service/authentication.service';
 import { UserService } from './auth/service/user.service';
 import { NavigationComponent } from './navigation/navigation.component';
+import { NotificationModule } from './notification-module/notification.module';
 import { UpperPanelComponent } from './upper-panel/upper-panel.component';
 
 @NgModule({
@@ -20,7 +21,8 @@ import { UpperPanelComponent } from './upper-panel/upper-panel.component';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NotificationModule
   ],
   providers: [
     AuthenticationGuard,
