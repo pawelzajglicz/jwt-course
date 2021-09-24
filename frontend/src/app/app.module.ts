200e5f2e88df99c8cb98c98ca35389e5cf6d2a81
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-09-24 23:02:21 +0200

    adding user component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,20 +7,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AuthenticationGuard } from './auth/guard/authentication.guard';
 import { AuthInterceptor } from './auth/interceptor/auth.interceptor';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { AuthenticationService } from './auth/service/authentication.service';
+import { UserComponent } from './auth/user/user.component';
 import { UserService } from './auth/service/user.service';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NotificationModule } from './notification-module/notification.module';
 import { UpperPanelComponent } from './upper-panel/upper-panel.component';
-import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NavigationComponent,
+    RegisterComponent,
     UpperPanelComponent,
-    RegisterComponent
+    UserComponent
   ],
   imports: [
     AppRoutingModule,
